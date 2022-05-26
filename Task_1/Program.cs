feb759b17e3bf9a464e42a7027470ff461a76aa5
Tatiana <Tatiana_novik@mail.ru>
2022-05-26 18:57:42 +0700

    HomeWork_4_Cs

---
+++ Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
+
+int Number (int A, int B)
+{
+    int result = A;
+    for (int i = 1; i < B; i++) 
+    {
+        result *= A;
+    } 
+    return result;
+}
+
+Console.WriteLine(Number(2,5));
---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int A (int a)
+{
+    int result = 0;
+    while (a / 10 > 0)
+    {
+        result = result + a % 10;
+        a = a / 10;
+    }
+    return result + a;
+}
+Console.WriteLine (A(223));
